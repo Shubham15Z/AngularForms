8028d7893d38b6318d1edcc7d1cf215e2b9d8089
Shubham Zungure <zungureshubham1@gmail.com>
2024-10-10 12:09:44 +0530

    added code for remplate driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-form';
+  @ViewChild('registrationForm') form: NgForm | undefined
+
+  firstName: string = '';
+  lastName: string = '';
+  emailAddress: string = '';
+
+  genders = [
+    {id: 'check-male', value: 'male', display: 'Male'},
+    {id: 'check-female', value: 'female', display: 'Female'},
+    {id: 'check-other', value: 'other', display: 'Prefer not to say'},
+  ]
+
+  onFormSubmitted() {
+    console.log(this.form);
+    console.log(this.form?.value.firstname);
+    console.log(this.form?.controls['firstname'].value);
+    console.log(this.form?.value.lastname);
+
+  }
 }
